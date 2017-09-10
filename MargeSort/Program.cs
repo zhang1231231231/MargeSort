de75f3e3de6eab2024c8418feb53b788fc1b2987
zhang1231231231 <625452708@qq.com>
2017-09-10 18:16:49 +0800

    归并排序算法的实现

--- MargeSort/Program.cs
+++ MargeSort/Program.cs
@@ -10,6 +10,66 @@ namespace MargeSort
     {
         static void Main(string[] args)
         {
+            int[] arr = { 4, 6, 2, 7, 8, 0, 9, 3, 1, 5 };
+            Sort(ref arr);
+        }
+
+        static void Sort(ref int[] arr)
+        {
+            int[] temp = new int[arr.Length];
+            Sort(ref arr, 0, arr.Length - 1, ref temp);
+            Print(temp);
+        }
+
+        static void Print(int[] arr)
+        {
+            foreach (var i in arr)
+            {
+                Console.WriteLine(i.ToString() + " ");
+            }
+        }
+
+        static void Sort(ref int[] arr, int low, int high, ref int[] temp)
+        {
+            if (low < high)
+            {
+                int mid = (low + high) / 2;
+                Sort(ref arr, low, mid, ref temp);
+                Sort(ref arr, mid + 1, high, ref temp);
+                Marge(ref arr, low, mid, high, ref temp);
+            }
+        }
+
+        static void Marge(ref int[] arr, int low, int mid, int high, ref int[] temp)
+        {
+            int i = low;
+            int j = mid + 1;
+            int k = 0;
+            while (i <= mid && j <= high)
+            {
+                if (arr[i] <= arr[j])
+                {
+                    temp[k++] = arr[i++];
+                }
+                else
+                {
+                    temp[k++] = arr[j++];
+                }
+            }
+            while (i <= mid)
+            {
+                temp[k++] = arr[i++];
+            }
+            while (j <= high)
+            {
+                temp[k++] = arr[j++];
+            }
+            k = 0;
+            //将temp中的元素全部拷贝到原数组中
+            while (low <= high)
+            {
+                arr[low++] = temp[k++];
+            }
         }
     }
 }
